bd4d3fd8e5d899415ac9d12dd458147b3549d208
Jesus <65581378+Jesuss27@users.noreply.github.com>
2022-03-07 18:22:50 -0800

    npm install react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation';
 import "./styles/main.scss"
+import {Switch, Route} from "react-router-dom"
+//import components
 import Landing from "./components/Landing.js"
 import About from "./components/About.js"
 import Portfolio from "./components/Portfolio.js"
 import Container from "react-bootstrap/Container"
 import SeeMore from './components/SeeMore';
 import InTouch from './components/InTouch';
+import Contact from './components/Contact';
+// styling and animation
 import { createGlobalStyle } from "styled-components";
-
+import {motion} from "framer-motion"
 
 function App() {
 
@@ -52,16 +56,44 @@ function App() {
     }
   
   `
+
+  const variants = {
+    visible: {
+      opacity:1,
+      y:0,
+      transition:{
+        when:"beforeChildren",
+        duration:1,
+      }
+    },
+    hidden:{
+      opacity:0,
+      y:50,
+    }
+  }
   return (
     <>
     <GlobalStyles />
     <Navigation />
     <Container>
-      <Landing />
-      <About />
-      <Portfolio />
-      <SeeMore />
-      <InTouch />
+      {/* motion container */}
+      <motion.div initial={"hidden"} animate={"visible"} variants={variants}>
+
+        <motion.div whileInView={"visible"} initial={"hidden"} variants={variants}>
+          <Landing />
+        </motion.div>
+
+        <motion.div  whileInView={"visible"} initial={"hidden"} variants={variants}>
+          <About />
+        </motion.div>
+
+        <motion.div whileInView={"visible"} initial={"hidden"} variants={variants}><Portfolio /></motion.div>
+        
+        <motion.div whileInView={"visible"} initial={"hidden"} variants={variants}><SeeMore /></motion.div>
+        <motion.div whileInView={"visible"} initial={"hidden"} variants={variants}><InTouch /></motion.div>
+        <motion.div whileInView={"visible"} initial={"hidden"} variants={variants}><Contact /></motion.div>
+
+      </motion.div>
     </Container>
     </>
   );
